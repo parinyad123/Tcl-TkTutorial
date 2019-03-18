be7161e3a0ec441c55b1a43c5768ed5a3e9887e0
parinyad123 <sci.equ@gmail.com>
2019-03-18 13:58:31 +0700

    tcl

---
+++ TclTutarial/4-SpecialVar.tcl
@@ -0,0 +1,19 @@
+
+# Tcl version
+puts $tcl_version
+
+# Tcl Package Path
+puts $tcl_pkgPath
+
+# Tcl library
+puts $tcl_library
+
+# Tcl Patch Level
+puts $tcl_pathchLevel
+
+# Tcl Precision
+puts $tcl_precision
+
+# Tcl Startup File
+puts $tcl_rcFileName
+
